52538da2cc51cfe225290ccd14b1cb29aa198c7f
Ankita Patil <ankita.patil@os3infotech.com>
2022-04-29 09:38:22 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 terraform init \
-    -backend-config="address=https://git.os3.coe.com.com/api/v4/projects/19/terraform/state/prod" \
-    -backend-config="lock_address=https://git.os3.coe.com/api/v4/projects/19/terraform/state/prod/lock" \
-    -backend-config="unlock_address=https://git.os3.coe.com/api/v4/projects/19/terraform/state/prod/lock" \
+    -backend-config="address=https://git.os3.coe.com.com/api/v4/projects/27/terraform/state/dev" \
+    -backend-config="lock_address=https://git.os3.coe.com/api/v4/projects/27/terraform/state/dev/lock" \
+    -backend-config="unlock_address=https://git.os3.coe.com/api/v4/projects/27/terraform/state/dev/lock" \
     -backend-config="username=ankita" \
-    -backend-config="password=G82Muf8d_dJ4MfZ3-s6F" \
+    -backend-config="password=RUT25jUvr8-4Nhmhq2WV" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
     -backend-config="retry_wait_min=5"
